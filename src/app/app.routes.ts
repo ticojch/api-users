c9529bdee06db911e875bd56b754e94f33448392
Roberto Jesus Chang Padilla <rjcp@ADATIO.LOCAL>
2025-04-04 14:07:31 +0200

    Redireccionar de / a /home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
     {path:'inicio', component: HomeComponent},
     {path:'users', component: UsersComponent},
     {path:'users/:id', component: UserDetailsComponent}
